7d3cf8f43cfe1db161ba305f0e5d7daf90d14203
anthony <anthonyribeiro299@gmail.com>
2023-05-10 21:53:42 -0300

    terceiro

--- 1021.lua
+++ 1021.lua
@@ -1,18 +1,11 @@
---nao entendi
-
-local vi = io.read():gsub("[.]","")
-vi = tonumber(vi)
-n, n2 = {100,50,20,10,5,2}, {100,50,25,10,5,1}
-
+v,n,m=io.read("l"):gsub("[.]",""),{100,50,20,10,5,2},{100,50,25,10,5,1}
 print("NOTAS:")
-
 for _,i in pairs(n) do
-    print(string.format("%d nota(s) de R$ %d.00", vi//(i*100), i))
-    vi = vi % (i * 100)
+	print(string.format("%d nota(s) de R$ %d.00",v//(i*100),i))
+	v=v%(i*100)
 end
-
 print("MOEDAS:")
-for _, j in pairs(n2) do
-    print(string.format("%d moeda(s) de R$ %.2f", vi // j, j/100))
-    vi = vi % j 
+for _,i in pairs(m) do
+	print(string.format("%d moeda(s) de R$ %.2f",v//i,i/100))
+	v=v%i
 end
